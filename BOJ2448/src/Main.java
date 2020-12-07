9a1bdaedff011b505a7289496c180386281d52f7
BHwi <43560497+BHwi@users.noreply.github.com>
2020-12-07 23:00:16 +0900

    201207 study upload
    stringbuilder

--- BOJ2448/src/Main.java
+++ BOJ2448/src/Main.java
@@ -1,38 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
-	public static char[][] map;
-	
+	public static boolean[][] map;
+	public static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
-		map = new char[n][2 * n - 1];
-		
-		for(int i = 0; i < n; i++) 
-			for(int j = 0; j < 2 * n - 1; j++)
-				map[i][j] = ' ';
-		
+		map = new boolean[n][2 * n - 1];
+
 		draw(0, n - 1, n);
 		
 		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < 2 * n - 1; j++)
-				System.out.print(map[i][j]);
-			System.out.println();
+			for(int j = 0; j < 2 * n - 1; j++) {
+				if(map[i][j]) sb.append("*");
+				else sb.append(" ");
+			}
+			sb.append("\n");
 		}
-
+		System.out.print(sb);
 	}
-	
+
 	public static void draw(int x, int y, int n) {
 		if (n == 3) {
-			map[x][y] = '*';
-			map[x + 1][y - 1] = '*';
-			map[x + 1][y + 1] = '*';
-			map[x + 2][y - 2] = '*';
-			map[x + 2][y - 1] = '*';
-			map[x + 2][y + 0] = '*';
-			map[x + 2][y + 1] = '*';
-			map[x + 2][y + 2] = '*';
+			map[x][y] = true;
+			map[x+1][y-1] = true;
+			map[x+1][y+1] = true;
+			map[x+2][y-2] = true;
+			map[x+2][y-1] = true;
+			map[x+2][y+0] = true;
+			map[x+2][y+1] = true;
+			map[x+2][y+2] = true;	
 			return;
 		}
 		
