70f9d99e31e77228b824d07b56eccd7c94ed590d
BHwi <43560497+BHwi@users.noreply.github.com>
2020-12-26 23:50:02 +0900

    201226 study upload

---
+++ BOJ11650/src/Main.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		ArrayList<Arr> list = new ArrayList<>();
+		
+		for(int i = 0; i < n; i++) {
+			String s = br.readLine();
+			StringTokenizer st = new StringTokenizer(s);
+			list.add(new Arr(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+		
+		Collections.sort(list);
+		
+		for(int i = 0; i < n; i++) {
+			sb.append(list.get(i) + "\n");
+		}
+		
+		System.out.print(sb);
+
+	}
+	
+	public static class Arr implements Comparable<Arr>{
+		int x, y;
+		
+		Arr(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int compareTo(Arr o) {
+			if(this.x != o.x)
+				return this.x - o.x;
+			else 
+				return this.y - o.y;
+		}
+		
+		@Override
+		public String toString() {
+			return this.x + " " + this.y;
+		}
+		
+	}
+
+}
